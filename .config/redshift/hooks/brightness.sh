d23f1009da1ccde5c97362593f3918d4f3db3455
jimver <jim.verheijde@hotmail.com>
2019-06-16 16:13:08 +0200

    redshift fix brightness

--- .config/redshift/hooks/brightness.sh
+++ .config/redshift/hooks/brightness.sh
@@ -5,21 +5,21 @@
 # Set brightness values for each status.
 # Range from 1 to 100 is valid
 brightness_day=100
-brightness_transition=50
-brightness_night=10
+brightness_transition=60
+brightness_night=20
 # Set fade time for changes to one minute
 fade_time=60000
 
 if [ "$1" = period-changed ]; then
 	case $3 in
 		night)
-			light -S $brightness_night -time $fade_time
+			light -S $brightness_night
 			;;
 		transition)
-			light -S $brightness_transition -time $fade_time
+			light -S $brightness_transition
 			;;
 		daytime)
-			light -S $brightness_day -time $fade_time
+			light -S $brightness_day
 			;;
 	esac
 fi
